22172562283840ca8319deb5be20e2ffbd3aa33c
kounotori3991 <84972573+kounotori3991@users.noreply.github.com>
2021-05-28 21:51:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,20 +2,61 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+client = commands.Bot(command_prefix='/')
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
+ID_silver = 842752442382286928
+ID_gold = 815767410908332053
+ID_platinum = 815637115736031232
+ID_diamond = 815636793574948865
+ID_master = 815808290998059028
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+async def remove_allrole(payload):
+    channel = client.get_channel(payload.channel)
+    member = payload.author
+    role = member.guild.get_role(ID_silver)
+    await member.remove_roles(role)
+    role = member.guild.get_role(ID_gold)
+    await member.remove_roles(role)
+    role = member.guild.get_role(ID_platinum)
+    await member.remove_roles(role)
+    role = member.guild.get_role(ID_diamond)
+    await member.remove_roles(role)
+    role = member.guild.get_role(ID_master)
+    await member.remove_roles(role)
 
+async def add_role(payload,role_id):
+    channel = client.get_channel(payload.channel)
+    member = payload.author
+    role = member.guild.get_role(role_id)
+    await member.add_roles(role)
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_ready():
+    print('ログインしました')
 
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == '/silver':
+        await remove_allrole(message)
+        await add_role(message,ID_silver)
 
-bot.run(token)
+    if message.content == '/gold':
+        await remove_allrole(message)
+        await add_role(message,ID_gold)
+
+    if message.content == '/platinum':
+        await remove_allrole(message)
+        await add_role(message,ID_platinum)
+
+    if message.content == '/diamond':
+        await remove_allrole(message)
+        await add_role(message,ID_diamond)
+
+    if message.content == '/master':
+        await remove_allrole(message)
+        await add_role(message,ID_master)
+
+client.run(TOKEN)
